b281e77f76da5d061d3e668f30226b0a61eb88e8
Tim De Paepe <tim.depaepe@gmail.com>
2022-10-03 12:18:28 +0200

    updated example to WOT

--- examples/BlinkOnWifiProp/BlinkOnWifiProp.ino
+++ examples/BlinkOnWifiProp/BlinkOnWifiProp.ino
@@ -8,93 +8,131 @@
    Copy and change it to build your first Arduino connected prop, you will
    only be limited by your imagination.
 
-   Requirements: 
-   - install ArduinoProps.zip library and dependencies (https://github.com/fauresystems/ArduinoProps)
-   - help: https://github.com/xcape-io/ArduinoProps/blob/master/help/ArduinoProps_sketch.md
+   Requirements:
+   - install ArduinoProps2.zip library and dependencies (https://github.com/gdm-webofthings/ArduinoProps2)
+   - help: https://github.com/gdm-webofthings/ArduinoProps2/blob/main/help/ArduinoProp_sketch.md
 */
+
 #include "ArduinoProps.h"
 
-// Setup your WiFi network
-const char* ssid = "";
-const char *passphrase = "";
-
 // If you're running xcape.io Room software you have to respect prop inbox/outbox
-// topicw syntax: Room/[escape room name]/Props/[propsname]/inbox|outbox
+// topic syntax: Room/[escape room name]/Props/[propsname]/inbox|outbox
 // https://xcape.io/go/room
+
+/* ---------------
+ * Configure your WiFi Prop here
+ * --------------- */
 
 WifiProp prop(u8"Arduino Blink", // as MQTT client id, should be unique per client for given broker
                   u8"Room/My room/Props/Arduino Blink/inbox",
                   u8"Room/My room/Props/Arduino Blink/outbox",
-                  "192.168.1.53", // your MQTT server IP address
+                  "192.168.50.131", // your MQTT server IP address
                   1883); // your MQTT server port;
 
-// Builtin led is not available with the shield
-#undef LED_BUILTIN
-#define LED_BUILTIN 8
+/* ---------------
+ * Define the variables (internal and Xcape.io variables)
+ * --------------- */
 
 PropDataLogical blinking(u8"blink", u8"yes", u8"no", true);
 PropDataLogical led(u8"led");
 PropDataText rssi(u8"rssi");
 
-void blink(); // forward
-PropAction blinkAction = PropAction(1000, blink);
+void blink(); // define your upcoming blink method
+PropAction blinkAction = PropAction(1000, blink); // this action will repeat every 1 second and will NOT block the code
 
-bool wifiBegun(false);
+bool wifiBegun(false); // this is an internal variable to use
+const char* ssid = "WOT"; // the SSID of the network
+const char *passphrase = "enterthegame"; // the passphras of the network
+const int propNumber = 15;
+
+/* ---------------
+ * Sketch Helpper Functions
+ * --------------- */
+
+/**
+ * Writes a header in the serial output
+ */
+void writeHeader(String title) {
+  Serial.println("--------");
+  Serial.println(title);
+  Serial.println("--------");
+  Serial.println("");
+}
+
+/* ---------------
+ * Do the setup process
+ * --------------- */
 
 void setup()
 {
   Serial.begin(9600);
 
+  writeHeader("Welcome to the Web Of Things Arduino Prop library!");
+
   prop.addData(&blinking);
   prop.addData(&led);
   prop.addData(&rssi);
-  
-  prop.begin(InboxMessage::run);
+
+  prop.begin(InboxMessage::run); // this will start a loop to check for MQTT messages
 
   pinMode(LED_BUILTIN, OUTPUT); // initialize digital pin LED_BUILTIN as an output
 
   // At this point, the broker is not connected yet
 }
 
-void loop()
-{
+/* ---------------
+ * Start the Arduino loop process
+ * --------------- */
+
+/**
+ * Validate the Wifi (do this in every loop)
+ */
+bool validateWifi() {
+  // if the Wifi was not initialized yet
   if (!wifiBegun) {
-    WiFi.begin(ssid, passphrase);
+    writeHeader("WiFi information");
+    WiFi.begin(ssid, passphrase); // start connecting
+    Serial.print("WiFiNNA firmware version: \t");
     Serial.println(WiFi.firmwareVersion());
     delay(250); // acceptable freeze for this prop (otherwise use PropAction for async-like behavior)
     // do static IP configuration disabling the dhcp client, must be called after every WiFi.begin()
-    String fv = WiFi.firmwareVersion();
-    if (fv.startsWith("1.0")) {
-      Serial.println("Please upgrade the firmware for static IP");
-      // see https://github.com/fauresystems/ArduinoProps/blob/master/WifiNinaFirmware.md
-    }
-    else {
-      //WiFi.config(IPAddress(192, 168, 1, 21), // local_ip
-      //	IPAddress(192, 168, 1, 1),  // dns_server
-      //	IPAddress(192, 168, 1, 1),  // gateway
-      //	IPAddress(255, 255, 255, 0)); // subnet
-    }
+
+    // uncomment this, to set the static IP address
+    WiFi.config(IPAddress(192, 168, 50, propNumber), // local_ip
+    IPAddress(192, 168, 50, 1),  // dns_server (sets to default gateway)
+    IPAddress(192, 168, 50, 1),  // gateway
+    IPAddress(255, 255, 255, 0)); // subnet
+
+    // if the Wifi is in connected state
     if (WiFi.status() == WL_CONNECTED) {
       wifiBegun = true;
+      Serial.print("Your local IP address: \t\t");
       Serial.println(WiFi.localIP());
+      Serial.print("Your subnet mask: \t\t");
       Serial.println(WiFi.subnetMask());
+      Serial.print("Gateway IP address: \t\t");
       Serial.println(WiFi.gatewayIP());
     } else {
       WiFi.end();
     }
-  } else if (wifiBegun && WiFi.status() != WL_CONNECTED) {
+  } else if (wifiBegun && WiFi.status() != WL_CONNECTED) { 
     WiFi.end();
     wifiBegun = false;
   }
-
-  prop.loop();
-
-  rssi.setValue(WiFi.RSSI() + String(" dBm")); // https://www.metageek.com/training/resources/understanding-rssi.html
-
-  led.setValue(digitalRead(LED_BUILTIN)); // read I/O
-
-  blinkAction.check(); // do your stuff, don't freeze the loop with delay() calls
 }
+
+void loop()
+{
+  validateWifi(); // validate the Wifi connection
+  prop.loop(); // start looping the prop (superpowers gottin from xcape.io)
+  rssi.setValue(WiFi.RSSI() + String(" dBm")); // https://www.metageek.com/training/resources/understanding-rssi.html
+  led.setValue(digitalRead(LED_BUILTIN)); // read I/O
+  blinkAction.check(); // do your stuff, don't freeze the loop with delay() calls (!)
+}
+
+/* ---------------
+ * The Xcape.io logic/code
+ * --------------- */
 
 void blink()
 {
